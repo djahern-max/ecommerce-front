e9138be573909a5d5ca4bfc274b97add9ec75c68
Dano <danielaherniv@gmail.com>
2020-05-25 23:00:40 -0400

    update

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -94,7 +94,7 @@ const Menu = ({ history }) => (
       {isAuthenticated() && (
         <li className='nav-item'>
           <span
-            className='nav-link hide-sm'
+            className='nav-link'
             style={{ cursor: 'pointer', color: '#ffffff' }}
             onClick={() =>
               signout(() => {
