ca7dcb43eac54b7f131badb1d268f66803ca19a1
Dano <danielaherniv@gmail.com>
2020-05-26 22:13:33 -0400

    updating user dashboard

--- src/user/UserDashboard.js
+++ src/user/UserDashboard.js
@@ -29,64 +29,80 @@ const Dashboard = () => {
 
   const userLinks = () => {
     return (
-      <div className='card'>
-        <h4 className='card-header'>User Links</h4>
-        <ul className='list-group'>
-          <li className='list-group-item'>
-            <Link className='nav-link' to='/cart'>
-              My Cart
-            </Link>
-          </li>
+      <div className='row'>
+        <div className='container mb-2'>
+          <div className='container-fluid mb-3'></div>
+          <div className='card'>
+            <h4 className='card-header'>User Links</h4>
+            <ul className='list-group'>
+              <li className='list-group-item'>
+                <Link className='nav-link' to='/cart'>
+                  My Cart
+                </Link>
+              </li>
 
-          <li className='list-group-item'>
-            <Link className='nav-link' to={`/profile/${_id}`}>
-              Update Profile
-            </Link>
-          </li>
-        </ul>
+              <li className='list-group-item'>
+                <Link className='nav-link' to={`/profile/${_id}`}>
+                  Update Profile
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     );
   };
 
   const userInfo = () => {
     return (
-      <div className='card mb-5'>
-        <h3 className='card-header'>User Information</h3>
-        <ul className='list-group'>
-          <li className='list-group-item'>{name}</li>
-          <li className='list-group-item'>{email}</li>
-          <li className='list-group-item'>
-            {role === 1 ? 'Admin' : 'Registered User'}
-          </li>
-        </ul>
+      <div className='row'>
+        <div className='container mb-2'>
+          <div className='container-fluid mb-3'></div>
+          <div className='card mb-5'>
+            <h3 className='card-header'>User Information</h3>
+            <ul className='list-group'>
+              <li className='list-group-item'>{name}</li>
+              <li className='list-group-item'>{email}</li>
+              <li className='list-group-item'>
+                {role === 1 ? 'Admin' : 'Registered User'}
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     );
   };
 
   const purchaseHistory = (history) => {
     return (
-      <div className='card mb-5'>
-        <h3 className='card-header'>Purchase history</h3>
-        <ul className='list-group'>
-          <li className='list-group-item'>
-            {history.map((h, i) => {
-              return (
-                <div>
-                  <hr />
-                  {h.products.map((p, i) => {
-                    return (
-                      <div key={i}>
-                        <h6>Product name: {p.name}</h6>
-                        <h6>Product price: ${p.price}</h6>
-                        <h6>Purchased date: {moment(p.createdAt).fromNow()}</h6>
-                      </div>
-                    );
-                  })}
-                </div>
-              );
-            })}
-          </li>
-        </ul>
+      <div className='row'>
+        <div className='container mb-2'>
+          <div className='container-fluid mb-3'></div>
+          <div className='card mb-5'>
+            <h3 className='card-header'>Purchase history</h3>
+            <ul className='list-group'>
+              <li className='list-group-item'>
+                {history.map((h, i) => {
+                  return (
+                    <div>
+                      {h.products.map((p, i) => {
+                        return (
+                          <div key={i}>
+                            <h6>Product name: {p.name}</h6>
+                            <h6>Product price: ${p.price}</h6>
+                            <h6>
+                              Purchased date: {moment(p.createdAt).fromNow()}
+                            </h6>
+                          </div>
+                        );
+                      })}
+                    </div>
+                  );
+                })}
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     );
   };
@@ -97,10 +113,12 @@ const Dashboard = () => {
       className='container'
     >
       <div className='row'>
-        <div className='col-3'>{userLinks()}</div>
-        <div className='col-9'>
-          {userInfo()}
-          {purchaseHistory(history)}
+        <div className='container mb-2'>
+          <div className='container-fluid mb-3'>
+            {userLinks()}
+            {userInfo()}
+            {purchaseHistory(history)}
+          </div>
         </div>
       </div>
     </Layout>
